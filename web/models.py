abff97c1681e158765d6dd9ea49080531a055e2e
Saeed <mahdyramazany@gmail.com>
2018-08-23 00:53:57 +0430

    expense model ro ezafe kardam01

--- web/models.py
+++ web/models.py
@@ -10,3 +10,9 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
