c224fbd5129c9761ca545549d9ae61b1b2cfc0d2
Saeed <mahdyramazany@gmail.com>
2018-08-24 19:16:59 +0430

    income as api ezafe shod

--- web/models.py
+++ web/models.py
@@ -4,7 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __unicode__(self):
+        return"{} token".format(self.user)
+
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
@@ -20,3 +27,5 @@ class Income(models.Model):
     user = models.ForeignKey(User)
     def __unicode__(self):
         return self.text
+    def __unicode__(self):
+        return "{}-{}-{}".format(self.text,self.date,self.amount)
